61638c7771f130d63a67ae5fd9ee5ca898313de8
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-06 19:30:14 +0100

    partial include template header

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from challenges.views import monthly_challenge_by_number, monthly_challenge, index
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
     path("<int:month>", monthly_challenge_by_number),
     path("<str:month>", monthly_challenge, name="month-challenge"),
 ]
