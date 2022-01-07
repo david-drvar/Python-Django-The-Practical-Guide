2dcd95cd5255ef3d0b1fbd03c45c705379576ac8
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-08 00:10:58 +0100

    more pages added - all posts and post-detail

--- my_website/blog/views.py
+++ my_website/blog/views.py
@@ -14,5 +14,5 @@ def all_posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
