b27a98a8e5db1a81abe8d3216a7b36604f0793d8
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-08 20:12:24 +0100

    new project initialization - book_store - learning django models

---
+++ book_store/book_outlet/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title}({self.rating})"
+
+
