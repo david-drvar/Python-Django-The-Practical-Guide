e85bc5baa239f2feab880295c2ed1c351f977545
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-09 00:12:01 +0100

    adding slug field and templates

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -7,8 +9,16 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default=False)
+    slug  = models.SlugField(default="", null=False, db_index=True)  # harry-potter-1
 
     def __str__(self):
         return f"{self.title}({self.rating})"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save( *args, **kwargs)  # make sure to built-in save method gets called
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
 
 
---
+++ book_store/book_outlet/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from book_outlet.views import index, book_detail
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("<slug:slug>", book_detail, name="book-detail")
+]
