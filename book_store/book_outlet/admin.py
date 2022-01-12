c9e3b461160f21129a8df660c4df51bbd7302f85
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-12 19:39:34 +0100

    one-to-many relation

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from book_outlet.models import Book
+from book_outlet.models import Book, Author
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -9,4 +9,10 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating")
     list_display = ("title", "author")
 
+class AuthorAdmin(admin.ModelAdmin):
+    #list_filter = ("author", "rating")
+    list_display = ("first_name", "last_name")
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
