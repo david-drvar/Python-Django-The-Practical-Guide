4a525539a0806e622bba83453e0cb8b7d8f9f449
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-17 22:12:42 +0100

    manual form validation

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from reviews.views import index
+from reviews.views import index, thank_you
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("thank-you", thank_you)
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "reviews/review.html")
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        if entered_username == "" and len(entered_username) >=100:
+            return render(request, "reviews/review.html", {
+                "has_error" : True
+            })
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
