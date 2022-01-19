172eba17143f6117ab4673de92e847f2b2a2714f
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-19 12:38:29 +0100

    FormView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -3,32 +3,41 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, FormView
 
 from reviews.forms import ReviewForm
 from reviews.models import Review
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
+#
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
+#
+#     def post(self, request):
+#         # existing_model = Review.objects.get(pk=1)
+#         # form = ReviewForm(request.POST, instance=existing_model)  INSTANCE IS FOR UPDATING!
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             # review = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
+#             # review.save()
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        # existing_model = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=existing_model)  INSTANCE IS FOR UPDATING!
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            # review = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
-            # review.save()
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
