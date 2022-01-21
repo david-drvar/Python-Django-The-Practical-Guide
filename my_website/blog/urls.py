41a594c5131f34cb88e68cd77933a5464eb61912
David Drvar <david.drvar.bogdanovic@gmail.com>
2022-01-22 00:26:16 +0100

    adding read later functionality

--- my_website/blog/urls.py
+++ my_website/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import StartingPageView, AllPostsView, SinglePostView
+from .views import StartingPageView, AllPostsView, SinglePostView, ReadLaterView
 
 urlpatterns = [
     path("", StartingPageView.as_view(), name="index"),
     path("all-posts", AllPostsView.as_view(), name="all-posts"),
-    path("posts/<slug:slug>", SinglePostView.as_view(), name="post-detail")  # posts/my-first-post
+    path("posts/<slug:slug>", SinglePostView.as_view(), name="post-detail"),
+    path("read-later", ReadLaterView.as_view(), name="read-later")  # posts/my-first-post
 ]
